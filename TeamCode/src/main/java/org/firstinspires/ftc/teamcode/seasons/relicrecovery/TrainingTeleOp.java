58d0a0e64de7e8b8b5ffb5efc939d28fa958b6ac
FTC Geared Up <ftcgearedup@gmail.com>
2018-01-07 18:07:30 -0500

    - Removed trainingRobot class files + added Head controls to existing TrainingTeleOp program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/TrainingTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/TrainingTeleOp.java
@@ -10,12 +10,13 @@ import com.qualcomm.robotcore.hardware.Gamepad;
  *
  * Created by daniel on 11/12/17.
  */
-@Disabled
-@TeleOp(name = "Teleopp", group = "teleop")
+
+@TeleOp(name = "Training TeleOp", group = "Promote Video")
 public class TrainingTeleOp extends LinearOpMode {
 
     private DcMotor right;
     private DcMotor left;
+    private DcMotor head;
     @Override
     public void runOpMode() throws InterruptedException {
         //set deadzones
@@ -25,17 +26,28 @@ public class TrainingTeleOp extends LinearOpMode {
 
       left = hardwareMap.dcMotor.get("l");
       right = hardwareMap.dcMotor.get("r");
-
+      head = hardwareMap.dcMotor.get("h");
         waitForStart();
         double speedLeft;
         double speedRight;
+        double speedHead;
 
         while (opModeIsActive()) {
             speedLeft = gamepad1.left_stick_x;
             speedRight = gamepad1.right_stick_x;
+            speedHead = 0;
+
+            if(gamepad1.left_bumper){
+                speedHead = -1;
+            } else if(gamepad1.right_bumper){
+                speedHead = 1;
+            } else {
+                speedHead = 0;
+            }
 
             left.setPower(speedLeft);
             right.setPower(speedRight);
+            head.setPower(speedHead);
 
         }
 
