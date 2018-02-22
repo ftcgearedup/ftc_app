6aad645c3e4a07cf26ddc45e04ef1e173c63d032
FTC Geared UP 6347 <ftcgearedup@gmail.com>
2018-02-22 18:19:20 -0500

    add new lift motor to work with the original motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/mechanism/impl/GlyphLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/mechanism/impl/GlyphLift.java
@@ -1,22 +1,15 @@
 package org.firstinspires.ftc.teamcode.seasons.relicrecovery.mechanism.impl;
 
-import android.text.method.Touch;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.mechanism.IMechanism;
 import org.firstinspires.ftc.teamcode.utils.JSONConfigOptions;
-
-import java.io.File;
 
 /**
  * The glyph lift mechanism collects glyphs with two grippers and is able to place them in the cryptobox.
@@ -31,7 +24,8 @@ public class GlyphLift implements IMechanism {
 
     private OpMode opMode;
 
-    private DcMotor liftMotor;
+    private DcMotor liftMotorLeft;
+    private DcMotor liftMotorRight;
     private DcMotor glyphIntakeMotor;
     private TouchSensor touchSensor;
     private ColorSensor colorSensor;
@@ -63,19 +57,20 @@ public class GlyphLift implements IMechanism {
         this.opMode = robot.getCurrentOpMode();
         HardwareMap hwMap = opMode.hardwareMap;
 
-        this.liftMotor = hwMap.dcMotor.get("lift");
+        this.liftMotorLeft = hwMap.dcMotor.get("liftml");
+        this.liftMotorRight = hwMap.dcMotor.get("liftmr");
         this.glyphIntakeMotor = hwMap.dcMotor.get("gm");
         this.touchSensor = hwMap.touchSensor.get("gts");
         this.colorSensor = hwMap.colorSensor.get("gcs");
 
         // reverse lift motor
-        liftMotor.setDirection(liftMotorDir);
+        liftMotorLeft.setDirection(liftMotorDir);
 
         // reverse glyph intake motor
         glyphIntakeMotor.setDirection(intakeMotorDir);
 
         // brake both motors
-        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public ColorSensor getColorSensor() {
@@ -98,13 +93,18 @@ public class GlyphLift implements IMechanism {
     }
 
     /**
-     * This method sets the power of the lift motor.
-     * The lift motor is the motor that raises and lowers the linear slides.
+     * This method sets the power of the Left lift motor.
+     * The Left lift motor is the motor that raises and lowers the linear slides.
+     * the right lift motor is one that assists the Left lift motor, and its direction must be
+     *  reversed because it is facing the opposite direction.
      *
-     * @param power the power to run the lift motor in a range of 1.0 to -1.0.
+     * @param power the power to run the Left lift motor in a range of 1.0 to -1.0.
      *              Negative values lower the lift and, likewise, positive values raise the lift.
      */
     public void setLiftMotorPower(double power) {
-        liftMotor.setPower(-power * (power < 0 ? MAX_LIFT_MOTOR_POWER_UP : MAX_LIFT_MOTOR_POWER_DOWN));
+        double powerCoefficient = (power < 0 ? MAX_LIFT_MOTOR_POWER_UP : MAX_LIFT_MOTOR_POWER_DOWN);
+
+        liftMotorLeft.setPower(-power * powerCoefficient);
+        liftMotorRight.setPower(power * powerCoefficient);
     }
 }
