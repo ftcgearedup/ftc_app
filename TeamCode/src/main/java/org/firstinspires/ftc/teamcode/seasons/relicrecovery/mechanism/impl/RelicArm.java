422125164cc0c093a81f90a836092fe7aaefab04
FTC Geared UP 6347 <ftcgearedup@gmail.com>
2018-02-23 18:21:26 -0500

    New relic arm hardware in RelicArm class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/mechanism/impl/RelicArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/mechanism/impl/RelicArm.java
@@ -1,81 +1,86 @@
 package org.firstinspires.ftc.teamcode.seasons.relicrecovery.mechanism.impl;
 
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.mechanism.IMechanism;
-import org.firstinspires.ftc.teamcode.seasons.relicrecovery.RelicRecoveryRobot;
 
 /**
  *
  */
 
 public class RelicArm implements IMechanism {
-    private DcMotor armMain;
+    private DcMotor armMotor;
 
-    private Servo gripper;
-    private CRServo armEx;
+    private Servo gripperServo;
+    private Servo armRotationServo;
 
     private boolean isClosed = false;
 
-
     public RelicArm(Robot robot) {
         HardwareMap hwMap = robot.getCurrentOpMode().hardwareMap;
-        this.armMain = hwMap.dcMotor.get("ram");
-        this.armMain.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        this.armMain.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        this.gripper = hwMap.servo.get("rag");
-        this.armEx = hwMap.crservo.get("rae");
-        this.armEx.setDirection(DcMotorSimple.Direction.REVERSE);
+        armMotor = hwMap.dcMotor.get("ram");
+        gripperServo = hwMap.servo.get("rag");
+        armRotationServo = hwMap.servo.get("rar");
+
+        // brake and run using encoder for
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // initially set the gripper servo position to open
+        openGrip();
     }
+
     /**
-     * Opens the Relic Arm Gripper
+     * Raises the relic arm rotation servo
+     */
+    public void raiseArmRotation() {
+        armRotationServo.setPosition(1.0);
+    }
+
+    /**
+     * Lowers the relic arm rotation servo
+     */
+    public void lowerArmRotation() {
+        armRotationServo.setPosition(0.3);
+    }
+
+    public void initializeArmRotationPosition() {
+        armRotationServo.setPosition(0);
+    }
+
+    /**
+     * Opens the relic arm gripper
      */
     public void openGrip() {
-        gripper.setPosition(0);
+        gripperServo.setPosition(0);
         isClosed = false;
     }
+
     /**
-     * Closes the Relic Arm Gripper
+     * Closes the relic arm gripper
      */
     public void closeGrip() {
-        gripper.setPosition(1);
+        gripperServo.setPosition(1.0);
         isClosed = true;
     }
+
     /**
-     * Sets power of the Main Relic Arm
+     * Sets power of the main relic arm motor
      *
      * @param power The power you want to set the Main Arm to move at
      */
     public void setArmMainPower(double power) {
-        armMain.setPower(power);
+        armMotor.setPower(power);
     }
-    /**
-     * Sets power of the Relic Arm Extension
-     *
-     * @param power The power you want to set the extension to move at
-     */
-    public void setArmExtensionPower(double power) {
-        armEx.setPower(power);
-    }
+
     /**
      * @return if the Relic Gripper is Closed or not (boolean)
      */
     public boolean isGripClosed(){
         return isClosed;
-    }
-    /**
-     * Initializes Relic Arm
-     */
-    public void initializeRelicArm(){
-        armEx.setPower(0);
-        armMain.setPower(0);
-        openGrip();
     }
 }
