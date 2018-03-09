01e8ccb8f04b491258709ca85af7fe58cf66c353
FTC Geared UP 6347 <ftcgearedup@gmail.com>
2018-03-09 18:14:23 -0500

    Add TimeDriveAlgorithm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/algorithms/impl/TimeDriveAlgorithm.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.algorithms.impl;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.mechanism.drivetrain.IDriveTrain;
+
+public class TimeDriveAlgorithm {
+    private OpMode opMode;
+    private IDriveTrain driveTrain;
+    private ElapsedTime timer;
+
+    public TimeDriveAlgorithm(Robot robot, IDriveTrain driveTrain) {
+        this.opMode = robot.getCurrentOpMode();
+        this.driveTrain = driveTrain;
+        this.timer = new ElapsedTime();
+    }
+
+    public void pivot(double speed, double milliseconds) {
+        timer.reset();
+
+        if(opMode instanceof LinearOpMode) {
+            LinearOpMode linearOpMode = (LinearOpMode)opMode;
+
+            while(linearOpMode.opModeIsActive() && timer.milliseconds() < milliseconds) {
+                driveTrain.pivot(speed);
+            }
+        }
+    }
+}
