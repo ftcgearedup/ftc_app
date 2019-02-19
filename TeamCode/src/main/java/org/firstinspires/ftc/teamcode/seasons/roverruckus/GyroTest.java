2cdfb90fd0232f36b4ef30fe7905205a98f429ad
Karina Davis <karinadavis@gmail.com>
2019-02-19 18:14:32 -0500

    playing with imu

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/roverruckus/GyroTest.java
@@ -0,0 +1,299 @@
+package org.firstinspires.ftc.teamcode.seasons.roverruckus;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.teamcode.mechanism.impl.BNO055IMUWrapper;
+import org.firstinspires.ftc.teamcode.seasons.roverruckus.utility.VufTFLiteHandler;
+
+import java.util.Locale;
+
+public class GyroTest extends VufTFLiteHandler {
+    private BNO055IMU imu;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+    private DcMotor frontLeft;
+    private DcMotor intake;
+    private DcMotor intakeLift;
+    private DcMotor lift;
+    private Servo lBucket;
+    private DcMotor hook;
+    private BNO055IMUWrapper bno055IMUWrapper;
+    private VuforiaNav useVuforia;
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
+
+    public ElapsedTime land = new ElapsedTime(ElapsedTime.MILLIS_IN_NANO);
+    boolean isSampling = true;
+
+    private double ticksPerRevNR20 = 560;
+    private double ticksPerRevNR40 = 1120;
+    private double ticksPerRevNR60 = 1680;
+
+    //The post gear box gear ratio.
+    private double gearRatio = 1.0;
+    //The circumference of the drive wheel.
+    private double wheelCircumference = 25.1327; // ??
+    //Formula to calculate ticks per centimeter for the current drive set up.FORWARDS/BACKWARD ONLY
+    private double ticksPerCm = (ticksPerRevNR20 * gearRatio) / wheelCircumference;
+    //Formula to calculate ticks per centimeter for the current drive set up.SIDEWAYS
+
+
+    //the code that moves the robot
+    public void runOpMode() throws InterruptedException {
+        initAll();
+        initHW();
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        // Start the logging of measured acceleration
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        // Loop and update the dashboard
+        while (opModeIsActive()) {
+            telemetry.update();
+        }
+
+        forward(30, .5);
+        telemetry.update();
+
+        land.reset();
+        while (land.milliseconds() <= 5000){
+            telemetry.update();
+        }
+
+        stopMotors();
+    }
+
+
+
+    public void initHW() {
+        // init the Wheels and other HardWare
+        frontRight = hardwareMap.dcMotor.get("fr");
+        backRight = hardwareMap.dcMotor.get("br");
+        frontLeft = hardwareMap.dcMotor.get("fl");
+        backLeft = hardwareMap.dcMotor.get("bl");
+        intake = hardwareMap.dcMotor.get("intake");
+        intakeLift = hardwareMap.dcMotor.get("intakeLift");
+        lift = hardwareMap.dcMotor.get("lift");
+        lBucket = hardwareMap.servo.get("lbucket");
+        hook = hardwareMap.dcMotor.get("hook");
+
+//        intake = hardwareMap.dcMotor.get("intake");
+//        intakeLift = hardwareMap.dcMotor.get("I-L");
+
+
+        // set wheel direction
+        this.setDirection();
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        this.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // set wheel power variables
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+//        intake.setPower(0);
+//        intakeLift.setPower(0);
+
+    }
+    public void setZeroPowBehv(DcMotor.ZeroPowerBehavior behv) {
+        frontLeft.setZeroPowerBehavior(behv);
+        frontRight.setZeroPowerBehavior(behv);
+        backLeft.setZeroPowerBehavior(behv);
+        backRight.setZeroPowerBehavior(behv);
+    }
+    public void setDirection() {
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void setDriveMode(DcMotor.RunMode mode) {
+        frontLeft.setMode(mode);
+        frontRight.setMode(mode);
+        backLeft.setMode(mode);
+        backRight.setMode(mode);
+    }
+
+    //for backwards use negative power
+    //Pass in centimeters.
+    public void forward(double targetDistance, double power) {
+        setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        double targetDistanceTicks = targetDistance * ticksPerCm;
+        double currentDistanceTicks = 0;
+        while ((currentDistanceTicks < targetDistanceTicks) && opModeIsActive()) {
+            telemetry.addData("Target pos ticks: ", targetDistanceTicks);
+            telemetry.addData("Target Distance:", targetDistance + "cm");
+            currentDistanceTicks = (frontRight.getCurrentPosition() +
+                    frontLeft.getCurrentPosition() +
+                    backRight.getCurrentPosition() +
+                    backLeft.getCurrentPosition()) / 4.0;
+            telemetry.addData("Current pos ticks Avg: ", currentDistanceTicks);
+            telemetry.addData("Current Distance cm", currentDistanceTicks / ticksPerCm);
+            telemetry.update();
+
+            frontLeft.setPower(power);
+            frontRight.setPower(power);
+            backLeft.setPower(power);
+            backRight.setPower(power);
+        }
+        stopMotors();
+    }
+    public void pivotCW(double degree, double power) {
+
+        setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        double currentDegree = 0;
+        while ((currentDegree < degree) && opModeIsActive()) {
+            currentDegree = (frontLeft.getCurrentPosition() + backLeft.getCurrentPosition())/2;
+            frontLeft.setPower(power);
+            frontRight.setPower(-power);
+            backLeft.setPower(power);
+            backRight.setPower(-power);
+        }
+        stopMotors();
+    }
+
+    public void pivotCC(double degree, double power) {
+
+        setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        double currentDegree = 0;
+        while (currentDegree < degree && opModeIsActive()) {
+            currentDegree = (frontRight.getCurrentPosition() + backRight.getCurrentPosition())/2;
+            frontLeft.setPower(-power);
+            frontRight.setPower(power);
+            backLeft.setPower(-power);
+            backRight.setPower(power);
+        }
+        stopMotors();
+    }
+    public void stopMotors() {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+    public void setLeftwardState(double power)
+    {
+        frontLeft.setPower(-power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(-power);
+    }
+
+    public void setRightwardState(double power)
+    {
+        frontLeft.setPower(power);
+        frontRight.setPower(-power);
+        backLeft.setPower(-power);
+        backRight.setPower(power);
+    }
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity  = imu.getGravity();
+        }
+        });
+
+        telemetry.addLine()
+                .addData("status", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("heading", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("grvty", new Func<String>() {
+                    @Override public String value() {
+                        return gravity.toString();
+                    }
+                })
+                .addData("mag", new Func<String>() {
+                    @Override public String value() {
+                        return String.format(Locale.getDefault(), "%.3f",
+                                Math.sqrt(gravity.xAccel*gravity.xAccel
+                                        + gravity.yAccel*gravity.yAccel
+                                        + gravity.zAccel*gravity.zAccel));
+                    }
+                });
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // Formatting
+    //----------------------------------------------------------------------------------------------
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+}
