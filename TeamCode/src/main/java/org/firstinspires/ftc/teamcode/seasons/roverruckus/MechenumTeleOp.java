d2b2068670b75987cdd7cdd787a0dc6fd09714d3
ftcgearedup <ftcgearedup@gmail.com>
2019-06-24 19:05:20 -0400

    MechenumTeleOp working with X pattern
    modified MechenumTeleOp because reasons unkown caused controls to be swapped about

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/roverruckus/MechenumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/roverruckus/MechenumTeleOp.java
@@ -9,17 +9,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+//works with X pattern mecanum wheel configuration, when in 'circle' pattern, strafing is reversed
+//NOTE: circle pattern is not prefered because the robot can be turned without the wheels moving.
 @TeleOp(name = "MecanumTeleOp", group = "TeleOp")
 public class MechenumTeleOp extends OpMode {
     private DcMotor frontRight;
     private DcMotor backRight;
     private DcMotor backLeft;
     private DcMotor frontLeft;
-    private DcMotor intake;
-    private DcMotor intakeLift;
-    private DcMotor lift;
-    private Servo lBucket;
-    private DcMotor hook;
+//    private DcMotor intake;
+//    private DcMotor intakeLift;
+//    private DcMotor lift;
+//    private Servo lBucket;
+//    private DcMotor hook;
     @Override
     public void init() {
         // init the motors
@@ -27,28 +29,28 @@ public class MechenumTeleOp extends OpMode {
         backRight = hardwareMap.dcMotor.get("br");
         frontLeft = hardwareMap.dcMotor.get("fl");
         backLeft = hardwareMap.dcMotor.get("bl");
-        intake = hardwareMap.dcMotor.get("intake");
-        intakeLift = hardwareMap.dcMotor.get("intakeLift");
-        lift = hardwareMap.dcMotor.get("lift");
-        lBucket = hardwareMap.servo.get("lbucket");
-        hook = hardwareMap.dcMotor.get("hook");
+//        intake = hardwareMap.dcMotor.get("intake");
+//        intakeLift = hardwareMap.dcMotor.get("intakeLift");
+//        lift = hardwareMap.dcMotor.get("lift");
+//        lBucket = hardwareMap.servo.get("lbucket");
+//        hook = hardwareMap.dcMotor.get("hook");
 
 
         // set wheel direction
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);  //originally forward
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);  //originally reverse
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);  //originally reverse
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);  //originally forward
         // set wheel power variables
         frontLeft.setPower(0);
         frontRight.setPower(0);
         backLeft.setPower(0);
         backRight.setPower(0);
         //init attachments
-        intake.setPower(0);
-        lift.setPower(0);
-        lBucket.setPosition(.5);
-        hook.setPower(0);
+//        intake.setPower(0);
+//        lift.setPower(0);
+//        lBucket.setPosition(.5);
+//        hook.setPower(0);
 
         // set deadzone
         gamepad1.setJoystickDeadzone(0.1f);
@@ -58,16 +60,16 @@ public class MechenumTeleOp extends OpMode {
         //the right Joystick controls movement, and the left controls turning.
         //in order to counteract the differences between the basic formula for strafing and the one for moving forwards and backwards,
         //we use trigonometry to derive relevant powers for each wheel tio move at the correct angles.
-        // translating polar coordanates of joyst   ick to polar coordinates of mechenum drive
-        double r = Math.hypot(gamepad1.right_stick_x, gamepad1.left_stick_y);
-        double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.right_stick_x) - Math.PI / 4;
+        // translating polar coordanates of joystick to polar coordinates of mechenum drive
+        double r = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
+        double robotAngle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x) - Math.PI / 4;
         double leftX = gamepad1.left_stick_x;
         final double fr = r * Math.cos(robotAngle) + leftX;
         final double fl = r * Math.sin(robotAngle) - leftX;
         final double bl = r * Math.sin(robotAngle) + leftX;
         final double br = r * Math.cos(robotAngle) - leftX;
 
-        double intakePower=0.0;
+//        double intakePower=0.0;
 
         if (gamepad1.left_trigger > 0) {
             frontRight.setPower(fr / 2);
@@ -87,34 +89,34 @@ public class MechenumTeleOp extends OpMode {
         }
         //attachments
 
-       if ( gamepad2.right_trigger == 1){
-            lBucket.setPosition(1);
-       } else if (gamepad2.left_trigger == 1){
-            lBucket.setPosition(0);
-       }
+//       if ( gamepad2.right_trigger == 1){
+//            lBucket.setPosition(1);
+//       } else if (gamepad2.left_trigger == 1){
+//            lBucket.setPosition(0);
+//       }
 
-       intakePower = gamepad2.left_stick_y;
-        intake.setPower(intakePower);
+//       intakePower = gamepad2.left_stick_y;
+//        intake.setPower(intakePower);
 
-        double liftPower;
-        liftPower = gamepad2.right_stick_y;
-        lift.setPower(liftPower);
+//        double liftPower;
+//        liftPower = gamepad2.right_stick_y;
+//        lift.setPower(liftPower);
 
-     if (gamepad2.dpad_up){
-         intakeLift.setPower(.7);
-     } else if (gamepad2.dpad_down) {
-         intakeLift.setPower(-.7);
-     }else {
-         intakeLift.setPower(0);
-     }
+//     if (gamepad2.dpad_up){
+//         intakeLift.setPower(.7);
+//     } else if (gamepad2.dpad_down) {
+//         intakeLift.setPower(-.7);
+//     }else {
+//         intakeLift.setPower(0);
+//     }
 
-     if(gamepad1.dpad_up){
-         hook.setPower(1);
-     }else if (gamepad1.dpad_down) {
-         hook.setPower(-1);
-     }else {
-         hook.setPower(0);
-     }
+//     if(gamepad1.dpad_up){
+//         hook.setPower(1);
+//     }else if (gamepad1.dpad_down) {
+//         hook.setPower(-1);
+//     }else {
+//         hook.setPower(0);
+//     }
         telemetry.addData("motor speeds","fl "+ fl + " fr "+fr + " bl "+ bl + " br "+ br);
         telemetry.addData("enc values",
                 "fl " + frontLeft.getCurrentPosition() +
